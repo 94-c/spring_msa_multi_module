703e645fb8cac03d640fce11189a51230459f2e4
94-c <hyeongwoo26@gmail.com>
2024-12-11 17:32:27 +0900

    [conf] 프로젝트 구성

---
+++ spring-api-service/spring-core-service/src/test/java/com/backend/core/SpringCoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
