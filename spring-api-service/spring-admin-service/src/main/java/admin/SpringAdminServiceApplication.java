3b7a113604ad97c5fa24fb5662493474bf71cf61
94-c <hyeongwoo26@gmail.com>
2024-12-13 16:41:25 +0900

    feat : core 서비스 스캔 적용

--- spring-api-service/spring-admin-service/src/main/java/admin/SpringAdminServiceApplication.java
+++ spring-api-service/spring-admin-service/src/main/java/admin/SpringAdminServiceApplication.java
@@ -2,8 +2,14 @@ package admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "com.backend.core", "admin"
+})
+@EnableJpaRepositories(basePackages = "com.backend.core")
+@EntityScan(basePackages = "com.backend.core")
 public class SpringAdminServiceApplication {
 
     public static void main(String[] args) {
--- spring-api-service/spring-user-service/src/main/java/user/SpringUserServiceApplication.java
+++ spring-api-service/spring-user-service/src/main/java/user/SpringUserServiceApplication.java
@@ -2,8 +2,14 @@ package user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "com.backend.core", "user"
+})
+@EnableJpaRepositories(basePackages = "com.backend.core")
+@EntityScan(basePackages = "com.backend.core")
 public class SpringUserServiceApplication {
 
     public static void main(String[] args) {
