0c5690cc379dfe75d7f5a390d45f9cbf986e58ae
94-c <hyeongwoo26@gmail.com>
2024-12-13 13:44:00 +0900

    feat : eureka 서비스 구축

---
+++ spring-eureka-service/src/test/java/com/backend/eureka/SpringEurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEurekaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
